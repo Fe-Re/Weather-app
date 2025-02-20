eccb390ff119421c355dc7b3321e7ac498806415
Fe-Re <Felixreisenhauer@googlemail.com>
2025-02-20 11:26:38 +0100

    remove unused weather icons and clean up icon imports in CurrentWeather component

--- weather-app/src/Components/CurrentWeather.js
+++ weather-app/src/Components/CurrentWeather.js
@@ -1,17 +1,3 @@
-import clearIcon from "../Image/weather-icons/day.svg";
-import cloudyIcon from "../Image/weather-icons/cloudy.svg";
-import rainIcon from "../Image/weather-icons/rainy-1.svg";
-import snowIcon from "../Image/weather-icons/snowy-1.svg";
-import thunderstormIcon from "../Image/weather-icons/thunder.svg";
-
-const weatherIcons = {
-  Clear: clearIcon,
-  Clouds: cloudyIcon,
-  Rain: rainIcon,
-  Snow: snowIcon,
-  Thunderstorm: thunderstormIcon,
-};
-
 export function CurrentWeather({ curWeather }) {
   if (!curWeather.main) return null;
   const weatherCondition = curWeather.weather[0].main;
