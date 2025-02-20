8e734f795f2baeecc7835e7bcf7fbb0421f1e7ec
Fe-Re <Felixreisenhauer@googlemail.com>
2025-02-20 10:57:53 +0100

    add weather icons and styles for current weather display

--- weather-app/src/Components/CurrentWeather.js
+++ weather-app/src/Components/CurrentWeather.js
@@ -1,14 +1,35 @@
+import clearIcon from "../Image/weather-icons/day.svg";
+import cloudyIcon from "../Image/weather-icons/cloudy.svg";
+import rainIcon from "../Image/weather-icons/rainy-1.svg";
+import snowIcon from "../Image/weather-icons/snowy-1.svg";
+import thunderstormIcon from "../Image/weather-icons/thunder.svg";
+
+const weatherIcons = {
+  Clear: clearIcon,
+  Clouds: cloudyIcon,
+  Rain: rainIcon,
+  Snow: snowIcon,
+  Thunderstorm: thunderstormIcon,
+};
+
 export function CurrentWeather({ curWeather }) {
+  if (!curWeather.main) return null;
+  const weatherCondition = curWeather.weather[0].main;
+  const weatherIcon = weatherIcons[weatherCondition] || clearIcon;
+
   return (
     <>
       {typeof curWeather.main != "undefined" ? (
         <div className="current-container">
+          <img
+            className="weather-icon"
+            src={weatherIcon}
+            alt={weatherCondition}
+          />
           <p>
             {curWeather.name}, {curWeather.sys.country}
           </p>
-
           <p>{curWeather.main.temp}°C</p>
-
           <p>{curWeather.weather[0].main}</p>
         </div>
       ) : (
